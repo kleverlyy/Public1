0d7750989ec31f17ab93e1ed8eea5e1091ff0848
Mark Shearer <shearer5050@gmail.com>
2018-03-19 21:44:37 -0400

    JavaFX Test

---
+++ HelloGit/src/net/ubilife/hellogit/TestFX_ND_A.java
@@ -0,0 +1,16 @@
+package net.ubilife.hellogit;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestFX_ND_A extends Application {
+
+  // start required by Application abstract class:
+  public void start (Stage stage) {
+    stage.show ();
+  } // end start
+ 
+  public static void main (String args []) {
+    launch (args);
+  } // end main
+} // end class TestFX_ND_A
