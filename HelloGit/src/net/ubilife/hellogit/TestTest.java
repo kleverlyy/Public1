1778b27247daf1470e3cc37f3dc024b4777fd1d8
Mark Shearer <shearer5050@gmail.com>
2018-04-24 00:42:37 -0400

    adf

--- HelloGit/src/net/ubilife/hellogit/TestTest.java
+++ HelloGit/src/net/ubilife/hellogit/TestTest.java
@@ -1,22 +1,22 @@
 package net.ubilife.hellogit;
 
+import java.io.File;
 import java.text.DecimalFormat;
+
+import javafx.scene.control.Label;
 
 public class TestTest {
 
 	public static void main(String args[]) {
-		double tempCompleted = 10; //number completed toward goal
-    	double tempGoal = 18; //goal to reach for task
-    	double percentageDone = 0.0; //percentage completed
-    	
-    	//if number completed > 0, then calculate percentage
-    	if (tempCompleted > 0) {
-    		percentageDone = tempCompleted / tempGoal;
-    		percentageDone = percentageDone * 100;
-    	}
-    	
-    	DecimalFormat dFormat = new DecimalFormat(".00");
-        
-        System.out.println(dFormat.format(percentageDone));
+		
+		Label userNameLabel;
+		String userName;
+		
+		String homePath = System.getProperty("user.home"); //pulls home directory
+		String[] arOfKeys = homePath.split(":?\\\\");
+		userName = arOfKeys[2];
+		
+		userNameLabel = new Label("userName");
+
 	}
 }
